030841712a1526cb680b2f2489e4de9b8e195737
Arthur Saprunov <Arthur.saprunov@student.howest.be>
2016-10-18 18:10:46 +0200

    Few updates
    ++ Added ImageLoader to GameUtils
    ++ Added zipLoader (test) to GameUtils
    ++ Added rotation support to Gamepad/Controller
    -- Removed RumbleTest (Rumble/vibration not supported by JInput)
    -- Removed  class Inputdevice

--- src/be/howest/input/Inputdevice.java
+++ src/be/howest/input/Inputdevice.java
@@ -1,25 +1,7 @@
 package be.howest.input;
 
-import java.util.*;
+import net.java.games.*;
 
-import net.java.games.input.Controller;
-import net.java.games.input.ControllerEnvironment;
-
-public abstract class Inputdevice {
-	private List<Controller> controllers = new ArrayList<>();
-	
-	
-	public Inputdevice(){
-		
-	}
-	
-	public void getAllInputdevices(){
-		Controller[] con = ControllerEnvironment.getDefaultEnvironment().getControllers();
-		for(Controller controller : con){
-			if(controller.getType() == Controller.Type.GAMEPAD){
-				controllers.add(controller);     
-			}
-		}
-	}
+public class Inputdevice {
 
 }
--- src/be/howest/util/GameUtils.java
+++ src/be/howest/util/GameUtils.java
@@ -1,5 +1,12 @@
 package be.howest.util;
 
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.zip.*;
+
+import javax.imageio.ImageIO;
+
 public class GameUtils {
 	
 	public static <T extends Comparable<T>> T clamp(T val, T min, T max) {
@@ -21,5 +28,49 @@ public class GameUtils {
             return t2;
         }
     }
+	
+	public static BufferedImage loadImage(String path){
+		BufferedImage image = null;
+		try{
+			image = ImageIO.read(new File(path));
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return image;
+	}
 
+	public static void test(){
+		ZipFile zipFile = null;
+		try {
+			zipFile = new ZipFile("resources\\resourcepack.zip");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+	    while(entries.hasMoreElements()){
+	        ZipEntry entry = entries.nextElement();
+	        try {
+				InputStream stream = zipFile.getInputStream(entry);
+				System.out.println(stream.toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	    
+	    try {
+			zipFile.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    
+	}
 }
