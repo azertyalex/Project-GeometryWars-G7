40e1371e8be3dc0bcafd165b17ab63098d199f56
QuintenDegraeve <Degraeve.Quinten@gmail.com>
2016-11-08 15:57:46 +0100

    Game states implemented
    Game states implemeted with menus: menu, drone_upgrade and power_shop

--- src/be/howest/game/Game.java
+++ src/be/howest/game/Game.java
@@ -13,6 +13,9 @@ import java.util.List;
 
 import javax.imageio.ImageIO;
 
+import be.howest.gfx.DroneUpgrade;
+import be.howest.gfx.Menu;
+import be.howest.gfx.PowerShop;
 import be.howest.gfx.Window;
 import be.howest.objects.*;
 import be.howest.util.GameLoop;
@@ -21,9 +24,9 @@ import be.howest.util.GameUtils;
 
 public class Game extends Canvas implements Runnable, GameLoop{
 
+	//720p res
 	private static final long serialVersionUID = -7744672260366215689L;
-	public static final int WIDTH = 720, HEIGHT = WIDTH / 12 *9;
-	
+	public static final int WIDTH = 1280, HEIGHT = WIDTH / 12 *9;	
 	public int frames = 0;
 	
 	private Graphics g;
@@ -33,10 +36,25 @@ public class Game extends Canvas implements Runnable, GameLoop{
 	
 	private Handler handler = new Handler();
 	
-	private List<GameObject> backgroundObjects = new ArrayList<>();
-	private List<GameObject> enemyObjects = new ArrayList<>();
-	private List<GameObject> playerObjects = new ArrayList<>();
-	private List<GameObject> hud = new ArrayList<>();
+	public enum STATE{
+		MENU,
+		PLAY,
+		DRONE_UPGRADE,
+		POWER_SHOP,
+		PAUSE,
+		CUSTOMIZATION,
+		OPTIONS
+	}
+	
+	private DroneUpgrade droneUpgrade;
+	private PowerShop powerShop;
+	
+	public STATE state = STATE.DRONE_UPGRADE;
+	
+	public static List<GameObject> backgroundObjects = new ArrayList<>();
+	public static List<GameObject> enemyObjects = new ArrayList<>();
+	public static List<GameObject> playerObjects = new ArrayList<>();
+	public static List<GameObject> hud = new ArrayList<>();
 	
 	
 	
@@ -47,15 +65,25 @@ public class Game extends Canvas implements Runnable, GameLoop{
 	}
 	
 	public Game(){
+		addAllObjects();
+
+		handler = new Handler();
+		powerShop = new PowerShop(this, handler);
+		droneUpgrade = new DroneUpgrade(this, handler, powerShop);
+		
+		//this.addKeyListener(new KeyInput(handler));
+		this.addMouseListener(droneUpgrade);
+
+		
 		new Window(WIDTH,HEIGHT,"Geometry Wars Howest",this);
 		
-		addAllObjects();
 		
-		
-		handler.addObject(backgroundObjects);
-		handler.addObject(enemyObjects);
-		handler.addObject(playerObjects);
-		handler.addObject(hud);
+		if (state == STATE.PLAY){
+			handler.addObject(backgroundObjects);
+			handler.addObject(enemyObjects);
+			handler.addObject(playerObjects);
+			handler.addObject(hud);
+		}
 	}
 
 	public synchronized void start(){
@@ -77,6 +105,7 @@ public class Game extends Canvas implements Runnable, GameLoop{
 
 	@Override
 	public void run() {
+		//FPS
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
@@ -90,6 +119,7 @@ public class Game extends Canvas implements Runnable, GameLoop{
 				tick();
 				delta--;				
 			}
+			
 			if(isRunning){
 				render(g);
 			}
@@ -100,16 +130,26 @@ public class Game extends Canvas implements Runnable, GameLoop{
 				//System.out.println("FPS: " + frames);
 				frames = 0;
 			}
-		}		
+		}	
+		//
+		
 	}
 	
 	
 	@Override
 	public void tick(){
-		handler.tick();
+			handler.tick();
 		
+		if(state == STATE.PLAY){
+			
+		} else if (state == STATE.DRONE_UPGRADE){
+			droneUpgrade.tick();
+		} else if (state == STATE.POWER_SHOP){
+			powerShop.tick();
+		}
 		
 	}
+	
 	
 	@Override
 	public void render(Graphics g){
@@ -120,17 +160,17 @@ public class Game extends Canvas implements Runnable, GameLoop{
 		}
 		g = bufferStrategy.getDrawGraphics();
 		
-		
-		//g.setColor(Color.black);
-		//g.fillRect(0, 0, WIDTH, HEIGHT);
-		
 		g.drawImage(GameUtils.loadImage("resources\\background\\background.jpg"), 0,0, WIDTH, HEIGHT, null);
 		
 		
 		
-		
-		
-		handler.render(g);
+		if (state == STATE.PLAY){
+			handler.render(g);
+		}else if (state == STATE.DRONE_UPGRADE){
+			droneUpgrade.render(g);
+		}else if (state == STATE.POWER_SHOP){
+			powerShop.render(g);
+		}
 		
 		g.dispose();
 		bufferStrategy.show();
--- src/be/howest/gfx/PowerShop.java
+++ src/be/howest/gfx/PowerShop.java
@@ -6,28 +6,30 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComboBox;
 
 import be.howest.game.Game;
 import be.howest.game.Game.STATE;
 import be.howest.objects.testObject;
+import be.howest.util.GameUtils;
 import be.howest.game.Handler;
 import be.howest.game.ID;
 
-public class Menu extends MouseAdapter{
-	public Rectangle playButton = new Rectangle(Game.WIDTH / 2, 150, 100, 50);
-	public Rectangle CustomizationButton = new Rectangle(Game.WIDTH / 2, 250, 100, 50);
-	public Rectangle OptionButton = new Rectangle(Game.WIDTH / 2, 350, 100, 50);
-	public Rectangle ExitButton = new Rectangle(Game.WIDTH / 2, 450, 100, 50);
+public class PowerShop extends MouseAdapter{
+	public Rectangle next = new Rectangle(Game.WIDTH / 2 - 250, 800, 500, 75);
 
 	private Game game;
 	private Handler handler;
 	
-	public Menu(Game game, Handler handler){
+	public PowerShop(Game game, Handler handler){
 		this.game = game;
 		this.handler = handler;
 	}
@@ -35,20 +37,27 @@ public class Menu extends MouseAdapter{
 	
 	public void render(Graphics g){
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(
+		        RenderingHints.KEY_TEXT_ANTIALIASING,
+		        RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
 		
-		Font fnt0 = new Font("Arial", Font.BOLD,50);
-		g.setFont(fnt0);
+		//Big Title
+		Font title = new Font("Arial", Font.BOLD,50);
+		g.setFont(title);
 		g.setColor(Color.white);
-		g.drawString("GEOMETRY WARS", 1280 / 2, 100);
+		g.drawString("INTERMISSION", 50, 75);
+		g.setColor(Color.red);
+		g.drawString("POWER SHOP", 49, 125);
+
 		
-		Font fnt1 = new Font("Arial", Font.BOLD, 30);
-		g.setFont(fnt1);
-		
-		g2d.draw(playButton);
-		g2d.draw(CustomizationButton);
-		g2d.draw(OptionButton);
-		g2d.draw(ExitButton);
-	
+		//UI - button
+		Font smaller_title = new Font("Arial", Font.BOLD, 30);
+
+		g2d.fill(next);
+		g.setColor(Color.black);
+		g.setFont(smaller_title);
+		g.drawString("Continue", Game.WIDTH / 2 - 30, 850);
 		
 		}
 	
@@ -73,7 +82,9 @@ public class Menu extends MouseAdapter{
 		System.out.println(mx);
 		System.out.println(my);
 		
-		if (mouseOver(mx, my, (1280 / 2), 150, 100, 50)){
+		//(Game.WIDTH / 2 - 250, 800, 500, 75);
+		if (mouseOver(mx, my, (Game.WIDTH / 2) -250, 800, 500, 75)){
+			System.out.println("PLAY GAME");
 			game.state = STATE.PLAY;
 			handler.addObject(new testObject(200,200,ID.Enemy));
 		}
